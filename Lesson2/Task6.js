1a1a32f189a4596ac8e5e8e26e7d903f002c25bc
RandomFloat <manzhurovam@mail.ru>
2022-02-06 19:31:48 +0500

    Практическое задание к уроку №2

--- Lesson2/Task6.js
+++ Lesson2/Task6.js
@@ -1,5 +1,7 @@
 ; "use strict";
-
+let unCorrect=false;
+do
+{
 let sumToPut=prompt("Введите целое число которое положить на счет:");
 let lastChar=sumToPut.substring(sumToPut.length-1,sumToPut.length);
 let integerSum=Number(sumToPut)
@@ -9,11 +11,13 @@ if(!isNaN(integerSum) && integerSum>0 && Math.abs(integerSum)!==Infinity )
     {
     case "1":
         alert(`Ваша сумма ${integerSum} рубль успешно зачислена`)
+        unCorrect=false;
         break;
     case "2":
     case "3":
     case "4":
         alert(`Ваша сумма ${integerSum} рубля успешно зачислена`)
+        unCorrect=false;
         break;
     case "5":
     case "6":
@@ -22,11 +26,14 @@ if(!isNaN(integerSum) && integerSum>0 && Math.abs(integerSum)!==Infinity )
     case "9":
     case "0":
         alert(`Ваша сумма ${integerSum} рублей успешно зачислена`)
+        unCorrect=false;
         break;
     }
+  
 }
 else
 {
     alert("Введено некорректное число(либо отрицательное либо нулевое либо просто набор букв) или вы нажали кнопку отмена");
+    unCorrect=true;
 }
-
+}while(unCorrect);
