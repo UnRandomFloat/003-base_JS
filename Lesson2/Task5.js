c41a76039f662f8118ccc2707bb24f583ee6dda1
RandomFloat <manzhurovam@mail.ru>
2022-02-06 19:09:54 +0500

    Практическое задание к уроку №2

---
+++ Lesson2/Task5.js
@@ -0,0 +1,20 @@
+;"use strict";
+
+function mathOperation(arg1, arg2, operation){
+    
+    switch(operation)
+    {
+    case "сумма":
+        return sumNums(arg1, arg2);
+    case "разность":
+        return subNums(arg1, arg2);
+    case "произведение":
+        return mulNums(arg1, arg2);
+    case "произведение":
+        return divNums(arg1, arg2);
+    }
+} 
+console.log(mathOperation(5,6,"сумма"));
+console.log(mathOperation(5,4,"разность"));
+console.log(mathOperation(5,10,"произведение"));
+console.log(mathOperation(5,1,"деление"));
---
+++ Lesson2/Task6.js
@@ -0,0 +1,32 @@
+; "use strict";
+
+let sumToPut=prompt("Введите целое число которое положить на счет:");
+let lastChar=sumToPut.substring(sumToPut.length-1,sumToPut.length);
+let integerSum=Number(sumToPut)
+if(!isNaN(integerSum) && integerSum>0 && Math.abs(integerSum)!==Infinity )
+{
+    switch(lastChar)
+    {
+    case "1":
+        alert(`Ваша сумма ${integerSum} рубль успешно зачислена`)
+        break;
+    case "2":
+    case "3":
+    case "4":
+        alert(`Ваша сумма ${integerSum} рубля успешно зачислена`)
+        break;
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
+        alert(`Ваша сумма ${integerSum} рублей успешно зачислена`)
+        break;
+    }
+}
+else
+{
+    alert("Введено некорректное число(либо отрицательное либо нулевое либо просто набор букв) или вы нажали кнопку отмена");
+}
+
